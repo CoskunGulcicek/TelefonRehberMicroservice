e4de5075cfd95d11b04e872d8b07250f4bd9d9c9
CoskunGulcicek <63802797+CoskunGulcicek@users.noreply.github.com>
2022-04-02 15:26:26 +0300

    information type entitisi eklendi

---
+++ Contact/Contact.Entities/Dtos/InformationType/InformationTypeAddDto.cs
@@ -0,0 +1,13 @@
+﻿using Contact.Entities.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contact.Entities.Dtos.InformationType
+{
+    public class InformationTypeAddDto : IDto
+    {
+    }
+}
